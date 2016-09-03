91e80036093a5d3a4694a354e87279b27e2b79e0
Sheng Wu <pkernoobie@gmail.com>
2016-09-03 02:10:59 +0000

    quiz 8 files, scraping lab

---
+++ labs/scraping/assignment/generate_random_html_dirs.py
@@ -0,0 +1,59 @@
+import random
+import string
+import os
+
+HTML = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>another random page</title>
+</head>
+<body>
+   {links}
+</body>
+</html>
+"""
+
+LINK = '<p><a href="http://sheng.io/fisk/labs/scraping/assignment/{url}">another page</a></p>'
+
+
+def rs():
+    N = 3
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(N))
+
+def rn():
+    return random.randint(2, 8)
+
+def create_dir(d):
+    os.makedir(d)
+
+
+saved = []
+for j in range(20):
+    pages = []
+    for x in range(20):
+        # make leaf index files
+        dirs = []
+        for y in xrange(rn()):
+            d = '/'.join(rs() for i in xrange(rn()))
+            os.makedirs(d)
+            dirs.append(d)
+            saved.append(d)
+            with open(d + '/index.html', 'w+') as f:
+                f.write(HTML.format(links=''))
+        d = '/'.join(rs() for i in xrange(rn()))
+        os.makedirs(d)
+        with open(d + '/index.html', 'w+') as f:
+            f.write(HTML.format(links='\n'.join(LINK.format(url=l) for l in dirs)))
+        pages.append(d)
+        saved.append(d)
+    d = '/'.join(rs() for i in xrange(rn()))
+    os.makedirs(d)
+    with open(d + '/index.html', 'w+') as f:
+        f.write(HTML.format(links='\n'.join(LINK.format(url=l) for l in pages)))
+    saved.append(d)
+    print LINK.format(url=d)
+
+
+with open('list.txt', 'w+') as savefile:
+    savefile.write('\n'.join(saved))
